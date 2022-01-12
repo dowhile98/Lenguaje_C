f9b968cad527521932523c66200e933fe641f1cd
StephanoEspinoza1999 <97580694+StephanoEspinoza1999@users.noreply.github.com>
2022-01-12 11:03:09 -0500

    CLASE 1
    STEPHANO

---
+++ Codigo/clase1_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*defines*/
+
+/*variables glabales*/
+float r=3.26;
+float A;
+const float PI = 3.1415;
+
+
+int main(void){
+    /*variables locales*/
+     
+     A= PI*r*r;
+     
+    printf("AREA-> %f\n",A);
+    /*salida del programa*/
+    system("pause");
+}
