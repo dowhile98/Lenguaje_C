22dfe2f59953dafec5ae98070302b327bedfb46d
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-01-12 10:45:56 -0500

    CLASE 1

---
+++ Codigo/clase1_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*defines*/
+
+/*variables glabales*/
+float r;
+float A;
+const float PI = 3.1415;
+
+
+int main(void){
+    /*variables locales*/
+
+    printf("AREA-> %f",A);
+    /*salida del programa*/
+    return 0;
+}
