750310525f1e237b9dbad1cb862884cf5e4d69a9
JhonFernando1 <jfernando.24@gmail.com>
2022-01-12 11:22:34 -0500

    Jhon Ejempl1
    Ejemplo 1

---
+++ Codigo/clase1_2.c
@@ -0,0 +1,18 @@
+// Ejercicio 1 JHON
+#include <stdio.h>
+#include <stdlib.h>
+/*defines*/
+
+/*variables glabales*/
+float r = 2;
+float A = 0;
+const float PI = 3.1415;
+
+
+int main(void){
+    /*variables locales*/
+    A=PI*r*r;
+    printf("AREA = %f",A);
+    /*salida del programa*/
+    return 0;
+}
