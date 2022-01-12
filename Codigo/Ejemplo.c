58bdecfd93fa85a400186bd35d64f1f6a9271dec
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-01-11 23:08:59 -0500

    Ejemplo

---
+++ Codigo/Ejemplo.c
@@ -0,0 +1,14 @@
+/*header files*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*main function*/
+int main(int argc, char *argv[]){
+
+    printf("Bienvenidos al curso: Lengauje C!!!\r\n");
+    printf("A aprender...\r\n");
+
+    system("PAUSE");
+    return 0;
+}
