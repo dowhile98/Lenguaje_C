f14ecc5f79c3f954e5c231f1e83364ea5ec9d3fd
jhchavezc <chavez.jeremyc@gmail.com>
2022-01-14 18:56:04 -0500

    Test

---
+++ Codigo/clase3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/*PORB: bit3 = 1, bit6 = 0*/
+/*PINB: Verificar si bit4 ==1
+  *entonces poner PORTB: bit7 = 1 y bit5 = 0*/
+uint8_t PORTB = 0b01110010;  //0b00000000 (0-255)
+uint8_t PINB = 0b11011001;
+
+int main(void){
+
+    
+    return 0;
+}
