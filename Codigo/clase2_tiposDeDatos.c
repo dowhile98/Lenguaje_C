aa6b8c053ea480a14365d51b8ef07c3d358d1d2c
jhchavezc <chavez.jeremyc@gmail.com>
2022-01-15 01:08:56 -0500

    s2_Actualizacion_Estudio

--- Codigo/clase2_tiposDeDatos.c
+++ Codigo/clase2_tiposDeDatos.c
@@ -1,5 +1,6 @@
 /*INCLUIMOS DE FICHEROS*/
 #include <stdio.h>
+#include <stdint.h>     //Definicion de datos enteros
 
 /*VARIABLES GLOBALES*/
 
@@ -14,9 +15,17 @@ int main(void){
     int c = 2147483647;                     //Variable tipo entero 4 bytes (-2147483648 0 hasta 2147483647)
     printf("El elemento c es: %i\n",c);
     unsigned int d = 2147483647;            //Variable entera sin signo 4 bytes (rango 0 hasta 2147483647)
-    
     printf("El elemento d es: %i\n",d);
-
+    long e = 2147483647;                    //Variable tipo entero 4 bytes (-2147483648 0 hasta 2147483647)
+    printf("El elemento e es: %li\n",e);
+    float f = 2147483647;                   //Variable tipo flotante 8 bytes (-2147483648 0 hasta 2147483647)
+    printf("El elemento f es: %f\n",f);
+    double g = 2147483647;                  //Variable tipo double 8 bytes (-2147483648 0 hasta 2147483647)
+    printf("El elemento g es: %lf\n",g);
+    int8_t h = 127;                        //Variable entera sin signo 8 bites (rango -128 hasta 127)  
+    printf("El elemento h es: %i\n",h);
+    uint8_t i = 255;                        //Variable entera sin signo 9 bites (rango 0 hasta 255)  
+    printf("El elemento i es: %i\n",i);
     /*RETORNO DE LA FUNCION PRINCIPAL*/
 
     return 0;
